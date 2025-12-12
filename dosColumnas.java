abb7f9c5785b8e835591fbd8ea60a7424a1900fa
Moisés <159391990+MoisesZION@users.noreply.github.com>
2025-12-12 14:59:39 -0500

    2columnas

---
+++ dosColumnas.java
@@ -0,0 +1,16 @@
+public class dosColumnas {
+
+    public static void main(String[] args) {
+
+        int a = 1;    
+        int b = 100; 
+
+        System.out.println("Columna 1    Columna 2");
+
+        while (a <= 100) {
+            System.out.println(a + "            " + b);
+            a++;
+            b--;
+        }
+    }
+}
---
+++ dosColumnas.py
@@ -0,0 +1,9 @@
+a = 1      # contador ascendente
+b = 100    # contador descendente
+
+print("Columna 1    Columna 2")
+
+while a <= 100:
+    print(a, "           ", b)
+    a += 1
+    b -= 1
